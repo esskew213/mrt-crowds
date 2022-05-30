c8e2712ec48297b9bc9549035cb70e7fa30e29b1
Sarahqtw <Sarahqtw@hotmail.com>
2022-05-30 10:01:56 +0800

    setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Switch from './components/Switch';
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+      <Switch />
+    </div>
+  );
+}
+
+export default App;
